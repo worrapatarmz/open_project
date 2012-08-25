e6636794af121fba02eabe626b89ac4d6e0ef9b1
worrapatarmz <worrapat857@gmail.com>
2012-08-25 14:00:02 +0700

    First Commit

---
+++ JLibs/src/AssertMethod.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public interface AssertMethod {
+    
+    public String AssertItems = null;
+    
+    public void  AssertMethod();
+    
+    public boolean setAssertList(String[][] itemName);
+    
+}
